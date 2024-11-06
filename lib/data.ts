9223adc11a09e6be934bb30f8e2da795d2ce3bb7
Abdulaziz <connect2abdulaziz@gmail.com>
2024-11-06 03:25:23 -0800

    [PW-4] page content skills and experiance swap CV updated and links added to project

--- lib/data.ts
+++ lib/data.ts
@@ -20,12 +20,12 @@ export const links = [
     hash: "#projects",
   },
   {
-    name: "Skills",
-    hash: "#skills",
-  },
-  {
     name: "Experience",
     hash: "#experience",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
   },
   {
     name: "Contact",
